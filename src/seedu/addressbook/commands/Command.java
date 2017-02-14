28dd6df0a9575f8a6b14070e4bbe8c5b4970a2f3
sherinatoh <sherinatohh@gmail.com>
2017-02-14 19:08:24 +0800

    add isMutating method

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -40,6 +40,11 @@ public abstract class Command {
      * Executes the command and returns the result.
      */
     public abstract CommandResult execute();
+    
+    /**
+     * Check if the data is mutated
+     */
+    public abstract boolean isMutating();
 
     /**
      * Supplies the data the command will operate on.
