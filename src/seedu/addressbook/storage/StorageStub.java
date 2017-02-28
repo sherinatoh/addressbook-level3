520763fc6ff4f67e4ae4f9b068ef3221f2d4d5a1
sherinatoh <sherinatohh@gmail.com>
2017-03-01 03:20:03 +0800

    modify LogicTest to use StorageStub

--- src/seedu/addressbook/storage/StorageStub.java
+++ src/seedu/addressbook/storage/StorageStub.java
@@ -1,15 +1,19 @@
 package seedu.addressbook.storage;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
+import java.io.Writer;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
 import seedu.addressbook.data.AddressBook;
@@ -73,7 +77,7 @@ public class StorageStub extends Storage {
 
 	@Override
 	public void save(AddressBook addressBook) throws StorageOperationException {
-
+		
 	}
 
 	@Override
